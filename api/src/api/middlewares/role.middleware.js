20c68fad7b6af9f9ed9eeec8185fa52fdebb5537
Rafacruiz <rafacruiz@gmail.com>
2026-04-13 21:10:31 +0200

    feat: add role middleware

---
+++ api/src/api/middlewares/role.middleware.js
@@ -0,0 +1,13 @@
+
+const roleMiddleware = (...roles) => {
+    
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            throw createHttpError(403, 'Unauthorized');
+        }
+
+        next();
+    }
+};
+
+export default roleMiddleware;
