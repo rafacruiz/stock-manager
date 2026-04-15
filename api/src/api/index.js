7ac76369de1def5308dbbcafccbf9bbc517af2db
Rafacruiz <rafacruiz@gmail.com>
2026-04-16 00:28:02 +0200

    feat: new router index.js Api

--- api/src/api/index.js
+++ api/src/api/index.js
@@ -1,15 +1,50 @@
 
 import { Router } from "express";
 
-import { errorHandler, schemaValidator } from './middlewares/errors.middleware.js';
+import { errorHandler } from './middlewares/errors.middleware.js';
 
-import authRoutes from "../routes/auth.router.js";
+import { authMiddleware } from './middlewares/auth.middleware.js';
+
+import authRoutes from "./routes/auth.router.js";
+import categoryRouter from "./routes/category.router.js";
+import stockRouter from "./routes/stock.router.js";
+import warehouseRouter from './routes/warehouse.router.js';
+import productRouter from './routes/product.router.js';
+import supplierRouter from "./routes/suppliers.router.js";
 
 const apiRoute = Router();
 
-apiRoute.use(schemaValidator);
+apiRoute.use(
+    "/auth", 
+    authRoutes
+);
 
-apiRoute.use("/auth", authRoutes);
+apiRoute.use(authMiddleware);
+
+apiRoute.use(
+    '/categories', 
+    categoryRouter
+);
+
+apiRoute.use(
+    '/warehouses',  
+    warehouseRouter
+);
+
+apiRoute.use(
+    '/products',  
+    productRouter
+);
+
+apiRoute.use(
+    '/stocks',
+    stockRouter
+);
+
+apiRoute.use(
+    '/suppliers',
+    supplierRouter
+);
 
 apiRoute.use(errorHandler);
 
